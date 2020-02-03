a744a9d4ae9668981413280f37c11bef911e21df
max_ner <maximuuum@yandex.ru>
2020-02-03 12:07:43 +0300

    вывод таблицы
    вывел таблицу категории

--- ASPOSystem/ClientApp/src/app/table-view/table-view.component.ts
+++ ASPOSystem/ClientApp/src/app/table-view/table-view.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-table-view',
   templateUrl: './table-view.component.html',
   styleUrls: ['./table-view.component.css']
 })
-export class TableViewComponent implements OnInit {
-
-  constructor() { }
+export class TableViewComponent  {
+    public categories: ICategory[];
+    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+        http.get<any>(baseUrl + 'api/Library/GetCategory').subscribe(result => {
+            this.categories = result as ICategory[];
+            console.log(this.categories);
+        }, error => console.error(error));
+    }
 
   ngOnInit() {
   }
 
+}
+
+interface ICategory {
+    IdCategory: string;
+    NameCategory: string;
 }
