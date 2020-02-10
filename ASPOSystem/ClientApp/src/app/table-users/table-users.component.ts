04db26a63b4c11022aaea7a06a7c3c39055747db
max_ner <maximuuum@yandex.ru>
2020-02-10 13:09:49 +0300

    CRUD таблицы Users
    Полный CRUD таблицы Users, с выводом реальных значений, а не GUID'ов

--- ASPOSystem/ClientApp/src/app/table-users/table-users.component.ts
+++ ASPOSystem/ClientApp/src/app/table-users/table-users.component.ts
@@ -3,6 +3,8 @@ import { IUsers, UsersService } from './UsersService';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import CustomStore from "devextreme/data/custom_store";
 import { DxDataGridComponent } from 'devextreme-angular';
+import { IPosts, PostService } from '../table-posts/PostService';
+import { IRoles, RoleService } from '../table-roles/RoleService';
 
 @Component({
     selector: 'app-table-users',
@@ -11,25 +13,39 @@ import { DxDataGridComponent } from 'devextreme-angular';
 })
 export class TableUsersComponent {
     public users: IUsers[];
+    public roles: IRoles[];
+    public posts: IPosts[];
     @ViewChild(DxDataGridComponent) dataGrid: DxDataGridComponent;
     store: any;
     headers: HttpHeaders;
-    constructor(private usersService: UsersService, public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
+    constructor(private usersService: UsersService, private roleService: RoleService, private postService: PostService,
+      public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
         sessionStorage.setItem("locale", 'ru');
+
         this.usersService.subject.subscribe(this.usersReceived);
         this.usersService.getUsers();
+
+        this.roleService.subject.subscribe(this.rolesReceived);
+        this.roleService.getRoles();
+
+        this.postService.subject.subscribe(this.postsReceived);
+        this.postService.getPosts();
+
         this.baseUrl = baseUrl;
         this.headers = new HttpHeaders().set('content-type', 'application/json');
-        this.store = new CustomStore({
+        setTimeout(() => {
+          this.store = new CustomStore({
             key: "idUser",
             load: () => this.users,
             insert: (values) => this.http.post<any>(this.baseUrl + 'Users/CreateUser', JSON.stringify(values as IUsers), { headers: this.headers }).subscribe(
-                () => { this.usersService.getUsers(); }),
+              () => { this.usersService.getUsers(); }),
             update: (key, values) =>
-                this.http.put<any>(this.baseUrl + 'Users/UpdateUser', JSON.stringify(values as IUsers), { headers: this.headers }).subscribe(
-                    () => { this.usersService.getUsers(); }),
+              this.http.put<any>(this.baseUrl + 'Users/UpdateUser', JSON.stringify(values as IUsers), { headers: this.headers }).subscribe(
+                () => { this.usersService.getUsers(); }),
             remove: (key) => this.http.delete<any>(this.baseUrl + 'Users/DeleteUser', { params: new HttpParams().set('idUser', key) }).subscribe(() => { this.usersService.getUsers(); })
-        });
+          });
+        }, 1000);
+        
     }
 
     onRowUpdating(e) {
@@ -45,8 +61,18 @@ export class TableUsersComponent {
         this.dataGrid.instance.refresh();
     }
 
-    ngOnInit() {
+    postsReceived = (data1: IPosts[]) => {
+        this.posts = data1;
+        this.dataGrid.instance.refresh();
+    }
 
+    rolesReceived = (data2: IRoles[]) => {
+        this.roles = data2;
+        this.dataGrid.instance.refresh();
+    }
+
+    ngOnInit() {
+      
     }
 
 }
