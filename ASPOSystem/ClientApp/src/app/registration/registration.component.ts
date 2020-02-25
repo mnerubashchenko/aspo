a916941b623c4a74bc85142f3cca3f9e3f490fab
max_ner <maximuuum@yandex.ru>
2020-02-25 13:45:11 +0300

    Регистрация с подтверждением пароля
    Регистрация с подтверждением пароля, но БЕЗ шифрования

--- ASPOSystem/ClientApp/src/app/registration/registration.component.ts
+++ ASPOSystem/ClientApp/src/app/registration/registration.component.ts
@@ -8,27 +8,29 @@ import { NgForm } from '@angular/forms';
   templateUrl: './registration.component.html'
 })
 export class RegistrationComponent {
-  invalidLogin: boolean;
+  check: boolean;
 
-  constructor(private router: Router, private http: HttpClient) { }
+  constructor(private router: Router, private http: HttpClient) {
+    this.check = true;
+  }
  
-  public login = (form: NgForm) => {
-    let credentials = JSON.stringify(form.value);
-    this.http.post("http://localhost:5000/Users/CreateUser", credentials, {
-      headers: new HttpHeaders({
-        "Content-Type": "application/json"
-      })
-    }).subscribe(response => {
-      //let token = (<any>response).token;
-      //localStorage.setItem("jwt", token);
-      //this.invalidLogin = false;
-      this.router.navigate(["/"]);
-    //}, err => {
-      //this.invalidLogin = true;
-    });
-  }
+    public registration = (form: NgForm) => {
+      this.check = (form.controls.passwordUser.value == form.controls.passwordConfirm.value) ? true : false;
+      let credentials = JSON.stringify(form.value);
 
-  isPasswordTrue() {
-
-  }
+        if (this.check) {
+          this.http.post("http://localhost:5000/Users/CreateUser", credentials, {
+            headers: new HttpHeaders({
+              "Content-Type": "application/json"
+            })
+          }).subscribe(response => {
+            //let token = (<any>response).token;
+            //localStorage.setItem("jwt", token);
+            //this.invalidLogin = false;
+            this.router.navigate(["/"]);
+            //}, err => {
+            //this.invalidLogin = true;
+          });
+        }
+    }
 }
