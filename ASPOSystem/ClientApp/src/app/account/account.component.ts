965c1119bfe044ea897d474f810900211b8a0074
Максим Нерубащенко <maximuuum@yandex.ru>
2020-03-12 16:51:40 +0300

    Изменение личных данных из личного кабинета
    - Добавлена возможность изменения данных о пользователе из личного кабинета
    
    Signed-off-by: Максим Нерубащенко <maximuuum@yandex.ru>

--- ASPOSystem/ClientApp/src/app/account/account.component.ts
+++ ASPOSystem/ClientApp/src/app/account/account.component.ts
@@ -1,39 +1,44 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Subject } from 'rxjs/Subject';
-import { UsersService } from '../table-users/UsersService';
+import { IUsers, UsersService } from '../table-users/UsersService';
 import { IRoles, RoleService } from '../table-roles/RoleService';
 import { IPosts, PostService } from '../table-posts/PostService';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+    selector: 'app-account',
+    templateUrl: './account.component.html',
+    styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
     public user: IUsers;
     public roles: IRoles[];
     public posts: IPosts[];
+    public flag: boolean = true;
+    public flag1: boolean = false;
     headers: HttpHeaders = new HttpHeaders({
         "Content-Type": "application/json"
-    }) ;
+    });
 
-    constructor(private usersService: UsersService, private roleService: RoleService, private postService: PostService, private http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
-    this.usersService.subjectAuth.subscribe(this.userAccountReceived);
-      this.usersService.getUserForAccount();
+    constructor(private usersService: UsersService, private roleService: RoleService,
+        private postService: PostService, private http: HttpClient, @Inject('BASE_URL') public baseUrl: string, private router: Router) {
+        this.usersService.subjectAuth.subscribe(this.userAccountReceived);
+        this.usersService.getUserForAccount();
 
-      this.roleService.subject.subscribe(this.rolesReceived);
+        this.roleService.subject.subscribe(this.rolesReceived);
         this.roleService.getRoles();
 
         this.postService.subject.subscribe(this.postsReceived);
         this.postService.getPosts();
     }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  userAccountReceived = (data: IUsers) => {
-      this.user = data;
+    userAccountReceived = (data: IUsers) => {
+        this.user = data;
     }
 
     rolesReceived = (data1: IRoles[]) => {
@@ -43,14 +48,27 @@ export class AccountComponent implements OnInit {
     postsReceived = (data2: IPosts[]) => {
         this.posts = data2;
     }
-}
 
-export interface IUsers {
-    IdUser: string;
-    NameUser: string;
-    MiddlenameUser: string;
-    LastnameUser: string;
-    LoginUser: string;
-    PostUser: string;
-    RoleUser: string;
+    private buttonIsPressed() {
+        this.flag = !this.flag;
+        this.flag1 = true;
+    }
+
+    private isChanged() {
+        return this.flag;
+    }
+
+    //private cancel() {
+    //    this.router.navigate(["/account"]);
+    //}
+
+    public account = (form: NgForm) => {
+        this.http.put<any>(this.baseUrl + "Users/UpdateUser", JSON.stringify(form.value as IUsers), {
+            headers: new HttpHeaders({
+                "Content-Type": "application/json"
+            })
+        }).subscribe();
+        this.flag1 = false;
+        this.flag = !this.flag;
+    }
 }
